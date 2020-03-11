9e856f93edb9117eb59e55601ca66b007c530e87
Kaike Carvalho Ventura <kcarvalv@everis.com>
2020-03-11 19:27:28 -0300

    Iniciado funções para gerar o boleto de acordo com os parametros

--- src/main/kotlin/br/com/kaikeventura/generators/model/BankSlip.kt
+++ src/main/kotlin/br/com/kaikeventura/generators/model/BankSlip.kt
@@ -1,5 +1,6 @@
 package br.com.kaikeventura.generators.model
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDate
 import java.time.LocalDateTime
 import javax.persistence.*
@@ -8,13 +9,15 @@ import javax.persistence.*
 data class BankSlip(
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
-        val id: Long,
+        val id: Long? = null,
         @Column(unique = true)
-        var readBarcode: String,
+        val typedBarcode: String? = null,
         @Column(unique = true)
-        val typedBarcode: String,
-        val bankCode: String,
-        val dueDate: LocalDate,
-        val creationDate: LocalDateTime,
-        val totalValue: Double
+        var readBarcode: String? = null,
+        val bankCode: String? = null,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+        val dueDate: LocalDate? = null,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss", timezone="GMT-3")
+        val creationDate: LocalDateTime? = null,
+        val totalValue: Double? = null
 )
--- src/main/kotlin/br/com/kaikeventura/generators/utils/BankSlipUtils.kt
+++ src/main/kotlin/br/com/kaikeventura/generators/utils/BankSlipUtils.kt
@@ -2,14 +2,39 @@ package br.com.kaikeventura.generators.utils
 
 import br.com.kaikeventura.generators.model.BankSlip
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.*
 
-class BankSlipUtils() {
+class BankSlipUtils()
 
-    fun generate(bankCode: String, dueDate: LocalDate, totalValue: Double): BankSlip? {
-        val bankSlip: BankSlip? = null
-        
-
-        return bankSlip
+    fun generateBankSlip(bankCode: String, dueDate: LocalDate, totalValue: Double): BankSlip {
+        return BankSlip(
+                null,
+                createTypedBarcode(bankCode),
+                null,
+                bankCode,
+                dueDate,
+                LocalDateTime.now(),
+                totalValue
+        )
     }
 
-}
+    fun generateGenericBarcode(): String{
+        val numbers = arrayOf<String>("1", "2", "3", "4", "5", "6", "7", "8", "9")
+        var bankSlipNumbers = String()
+        for(n in 0..47){
+            bankSlipNumbers += numbers[(Math.random()*numbers.size).toInt()]
+        }
+        return bankSlipNumbers
+    }
+
+    fun createTypedBarcode(bankCode: String): String{
+        var genericBarcode = generateGenericBarcode()
+        var barcodeWithBankCode = bankCode+genericBarcode.substring(3, 48)
+
+        return barcodeWithBankCode
+    }
+
+    fun main(args : Array<String>) {
+        createTypedBarcode("341")
+    }
